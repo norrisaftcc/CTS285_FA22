f7609edb4599fd673504ea72295d3636da09e89c
norrisa <norrisa@faytechcc.edu>
2022-10-09 20:02:59 -0400

    added memory bank

--- CppMan/procman.cpp
+++ CppMan/procman.cpp
@@ -50,7 +50,9 @@ int ui_memory_bank();
 // Data methods
 problem read_problem();
 string textify_problem(problem p);
+string textify_problem_no_answer(problem p);
 bool check_problem(problem p);
+bool check_answer(problem p, int answer);
 // Logic methods
 
 
@@ -142,7 +144,65 @@ int ui_answer_checker() {
 }
 
 int ui_memory_bank() {
-    cout << "Memory Bank -- Unimplemented" << endl << endl;
+    /*
+    ui_memory_bank() -- handles Memory Bank feature
+    memory bank works as follows:
+    1. User enters a problem
+    2. Problem is added to the memory bank list
+    3. User can view the memory bank list
+    4. User can solve a problem from the memory bank list
+    */
+    
+    cout << "Memory Bank" << endl;
+    vector<problem> memoryBank;
+    string command;
+    do {
+        cout << "Memory Bank Menu" << endl;
+        cout << "1. Enter a problem" << endl;
+        cout << "2. View memory bank" << endl;
+        cout << "3. Solve a problem" << endl;
+        cout << "0. Exit" << endl;
+        cout << "Enter a command: " << endl;
+        
+        cin >> command;
+        if (command == "1") {
+            problem p = read_problem();
+            cout << "You entered: " << textify_problem(p) << endl;
+            memoryBank.push_back(p);
+        }
+        else if (command == "2") {
+            for (problem p : memoryBank) {
+                cout << textify_problem(p) << endl;
+            }
+        }
+        else if (command == "3") {
+            // copilot did this one from scratch. good copilot.
+            cout << "Solve a problem" << endl;
+            cout << "Enter a problem number: " << endl;
+            int problemNumber;
+            cin >> problemNumber;
+            problem p = memoryBank.at(problemNumber - 1);
+            cout << "Solving: " << textify_problem(p) << endl;
+            cout << "Problem is:" << textify_problem_no_answer(p) << endl;
+            int userAnswer;
+            cout << "?";
+            cin >> userAnswer;
+            bool isCorrect = check_answer(p, userAnswer);
+            if (true == isCorrect) {
+                cout << endl << "*** Correct! ***" << endl << endl;
+            }
+            else {
+                cout << endl << "*** Incorrect! ***" << endl << endl;
+            }
+        }
+        else if (command == "0") {
+            cout << "Exiting memory bank" << endl << endl;
+        }
+        else {
+            cout << "Invalid command" << endl;
+        }
+    
+    } while (command != "0");
     return 0;
 }
 
@@ -168,6 +228,12 @@ problem read_problem() {
 string textify_problem(problem p) {
     string text;
     text = to_string(p.operator1) + " " + p.operand + " " + to_string(p.operator2) + " = " + to_string(p.answer);
+    return text;
+}
+
+string textify_problem_no_answer(problem p) {
+    string text;
+    text = to_string(p.operator1) + " " + p.operand + " " + to_string(p.operator2) + " = ";
     return text;
 }
 
@@ -200,5 +266,35 @@ bool check_problem(problem p) {
 
     // i spent 15 minutes figuring out why my answers were wrong
     // i was returning result, not isCorrect (and it was getting munged into a bool)
+    return isCorrect;
+}
+
+
+bool check_answer(problem p, int answer) {
+    // returns true if the user answer is correct
+
+    int result = 0;
+    bool isCorrect = 0;
+    if (p.operand == "+") {
+        result = p.operator1 + p.operator2;
+    }
+    else if (p.operand == "-") {
+        result = p.operator1 - p.operator2;
+    }
+    else if (p.operand == "*") {
+        result = p.operator1 * p.operator2;
+    }
+    else if (p.operand == "/") {
+        result = p.operator1 / p.operator2;
+    }
+    else {
+        cout << "Invalid operand" << endl;
+        return false;
+    }
+    //cout << "DEBUG: result = " << result << " p.answer = " << p.answer << endl;
+    if (result == answer) {
+        isCorrect = 1; // true
+    }
+    
     return isCorrect;
 }
